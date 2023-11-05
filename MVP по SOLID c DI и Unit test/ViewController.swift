600c3ce7eeb660a5e4f602deb8091de5461ddb61
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-11-05 10:55:57 +0300

    Initial Commit

---
+++ MVP по SOLID c DI и Unit test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP по SOLID c DI и Unit test
+//
+//  Created by Goodwasp on 05.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
